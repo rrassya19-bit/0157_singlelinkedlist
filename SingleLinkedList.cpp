aaeaca286ac012089692a657f3dcad6e27010d44
rrassya19-bit <rrassys19@gmail.com>
2026-04-24 16:48:57 +0700

    Tambah fungsi traverse untuk menampilkan semua data dalam linked list

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -89,4 +89,23 @@ public:
         delete current;
         return true;
     }
+
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList Kosong\n";
+        }
+        else
+        {
+            cout << "\nData didalam list adalah: \n";
+            Node *currentNode = START;
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
+    }
 };
