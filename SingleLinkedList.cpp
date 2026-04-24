79915ac6184958f9b18b50c818155c17e29f8fb9
rrassya19-bit <rrassys19@gmail.com>
2026-04-24 16:48:17 +0700

    Tambah fungsi Search untuk mencari data dalam linked list

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -61,4 +61,17 @@ public:
     {
         return START == NULL;
     }
+
+    bool Search(int nim, Node **previous, Node **current)
+    {
+        *previous = START;
+        *current = START;
+
+        while ((*current != NULL) && (nim != (*current)->noMhs))
+        {
+            *previous = *current;
+            *current = (*current)->next;
+        }
+        return (*current != NULL);
+    }
 };
