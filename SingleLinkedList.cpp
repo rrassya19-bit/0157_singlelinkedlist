d209243c18efceecf91b6b2fe3d13e340e4cc28b
rrassya19-bit <rrassys19@gmail.com>
2026-04-24 16:47:35 +0700

    Tambah fungsi addNode untuk menambahkan data secara terurut

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -17,4 +17,43 @@ public:
     {
         START = NULL;
     }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukan nomor mahasiswa; ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node();
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if ((START != NULL) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikasi noMhs tidak diizinkan\n";
+                return;
+            }
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
+
+        Node *previous = START;
+        Node *current = START;
+
+        while ((current != NULL) && (nim >= current->noMhs))
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diizinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
 };
